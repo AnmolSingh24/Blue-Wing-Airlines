69f36eb008c210ae74dc13f4aa0839aa6576902b
Anmol Singh <as0796348@gmail.com>
2024-04-09 20:10:55 +0530

    Changes added

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../../assets/images/BlueWingAirwaysLogo.png"
+import logo from "../../assets/images/BlueWingLogo.jpg"
 import About from '../../pages/about/About'
 import Destination from '../../pages/destination/Destination'
 import BookFlights from '../../pages/book/BookFlights'
@@ -12,9 +12,9 @@ import Profile from "../../pages/profile/Profile"
 const Header = () => {
     return (
         <>
-            <div className='h-12 flex items-center justify-between ml-10 text-blue-500'>
-                <img src={logo} alt="logo" className='w-12' />
-                <p className='-ml-[16rem]'>Blue Wing Airways</p>
+            <div className='h-12 flex items-center justify-between ml-10 text-blue-500 mt-2'>
+                <img src={logo} alt="logo" className='w-20' />
+                <p className='-ml-[13rem] uppercase'><span className='font-extrabold uppercase text-blue-800'>Blue</span> Wing <span className='normal-case'>Tours&Travels</span></p>
                 <div className='flex gap-16 text-lg font-semibold pr-16 cursor-pointer'>
                     <About />
                     <Destination />
--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from "../../assets/images/BlueWingLogo.jpg"
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div>
+        <div className="flex items-center justify-center mt-0">
+          <img src={logo} alt="Logo" className='w-12' />
+        </div>
+
+      </div>
+    </>
   )
 }
 
