f2c0f3cfe3c1d18b0653d9257137181404eeff86
Anmol Singh <as0796348@gmail.com>
2024-04-14 09:39:17 +0530

    changes added

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CgProfile } from "react-icons/cg";
 
 const Header = () => {
     return (
         <div className='h-12 flex items-center justify-between ml-10 text-blue-500 mt-2'>
             <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1712760856/BlueWingLogo_keleuk.png" alt="logo" className='w-20' />
             <p className='-ml-[13rem] uppercase'><span className='font-extrabold uppercase text-blue-800'>Blue</span> Wing Airlines</p>
-            <div className='flex gap-12 text-lg font-semibold pr-16 cursor-pointer'>
+            <div className='flex items-center justify-center gap-12 text-lg font-semibold pr-16 cursor-pointer'>
                 <Link to="/about">About</Link>
                 <Link to="/destination">Destination</Link>
                 <Link to="/searchflights">Flights</Link>
@@ -14,7 +15,7 @@ const Header = () => {
                 <Link to="/etickets">E-Tickets</Link>
                 <Link to="/flightstatus">Flight Status</Link>
                 <Link to="/support">Support</Link>
-                <Link to="/profile">Profile</Link>
+                <Link to="/profile"><CgProfile className='w-6 h-6' /></Link>
             </div>
         </div>
     );
