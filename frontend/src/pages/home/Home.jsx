769d178b11e09abcbd2d46c99f9326531fa974b1
Anmol Singh <as0796348@gmail.com>
2024-04-14 20:26:53 +0530

    changes added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -48,6 +48,52 @@ function CarouselSlider() {
     );
 }
 
+const ReadyToTravel = ({ img1, img2, img3, link1, link2, link3 }) => {
+    return (
+        <div className="travel-img flex justify-center items-center mt-8">
+            <div>
+                <img src={img1} />
+                <div>
+                    <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link1}</a>
+                </div>
+            </div>
+            <div className="ml-4">
+                <img src={img2} alt="before-you-fly" className='w-[38rem] h-60 -mb-4 rounded-lg' />
+                <div>
+                    <a href="/Travel/Documents/Visa.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link2}</a>
+                </div>
+                <img src={img3} alt="airport-information" className='w-[38rem] h-52 mt-8 -mb-4 rounded-lg' />
+                <div>
+                    <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link3}</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const BlueWingCabin = ({ img1, img2, img3, link1, link2, link3 }) => {
+    return (
+        <div className="travel-img flex justify-center items-center mt-8">
+            <div>
+                <img src={img1} alt="/generous-baggage" className="w-[38rem] h-[35rem] -mb-4 rounded-lg" />
+                <div className="btn">
+                    <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link1}</a>
+                </div>
+            </div>
+            <div className="ml-4">
+                <img src={img2} alt="before-you-fly" className='w-[38rem] h-[15.8rem] -mb-4 rounded-lg' />
+                <div className="btn">
+                    <a href="/Travel/Documents/Visa.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link2}</a>
+                </div>
+                <img src={img3} alt="airport-information" className='w-[38rem] h-[15.8rem] mt-8 -mb-4 rounded-lg' />
+                <div className="btn">
+                    <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">{link3}</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 const AsideSection = () => {
     return (
         <div className='fixed'>
@@ -61,83 +107,48 @@ const AsideSection = () => {
     );
 };
 
-const PrepareToTravel = () => {
+const TravelInfo = () => {
     return (
         <div className="text-center pt-2">
             <CarouselSlider />
             <p className="text-blue-600 text-2xl pt-16"><span className='font-bold text-blue-800'>Ready</span> To Travel</p>
             <h4 className="text-gray-500 text-lg">From booking services to help in travel plans, you can expect a lot along your journey.</h4>
-            <div className="travel-img flex justify-center items-center mt-8">
-                <div>
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484896/generous-baggage_mffl4g.webp" alt="/generous-baggage" className="w-[38rem] h-[25rem] -mb-4 rounded-lg" />
-                    <div className="btn">
-                        <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Learn More</a>
-                    </div>
-                </div>
-                <div className="ml-8">
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484943/tips-reminders_pckz3y.webp" alt="before-you-fly" className='w-[38rem] h-60 -mb-4 rounded-lg' />
-                    <div className="btn">
-                        <a href="/Travel/Documents/Visa.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Learn More</a>
-                    </div>
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484844/airport-information_ibqiq3.webp" alt="airport-information" className='w-[38rem] h-60 mt-8 -mb-4 rounded-lg' />
-                    <div className="btn">
-                        <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Learn More</a>
-                    </div>
-                </div>
-            </div>
+
+            <ReadyToTravel
+                img1="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484896/generous-baggage_mffl4g.webp"
+                link1="Learn More"
+                img2="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484943/tips-reminders_pckz3y.webp"
+                link2="Learn More"
+                img3="https://res.cloudinary.com/djdblxcxt/image/upload/v1695484844/airport-information_ibqiq3.webp"
+                link3="Learn More"
+            />
 
             <p className="text-blue-600 text-2xl pt-16"><span className='font-bold text-blue-800'>BLUE</span> WING CABIN</p>
-            <div className="travel-img flex justify-center items-center mt-8">
-                <div>
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1712805043/business-class-cabin_vs0fmv.jpg" alt="/generous-baggage" className="w-[38rem] h-[35rem] -mb-4 rounded-lg" />
-                    <div className="btn">
-                        <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Know More</a>
-                    </div>
-                </div>
-                <div className="ml-8">
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1712892777/premium-economy-class_valrld.png" alt="before-you-fly" className='w-[38rem] h-[15.8rem] -mb-4 rounded-lg' />
-                    <div className="btn">
-                        <a href="/Travel/Documents/Visa.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Know More</a>
-                    </div>
-                    <img src="https://res.cloudinary.com/djdblxcxt/image/upload/v1712764058/standard-economy-cabin_nkmmxk.png" alt="airport-information" className='w-[38rem] h-[15.8rem] mt-8 -mb-4 rounded-lg' />
-                    <div className="btn">
-                        <a href="/Travel/Baggage/Baggage.html" className="px-10 py-2 text-lg font-bold bg-blue-200 text-blue-900 border-4 border-white hover:bg-blue-700 hover:text-white transition duration-500 rounded-lg">Know More</a>
-                    </div>
-                </div>
-            </div>
+            <BlueWingCabin
+                img1="https://res.cloudinary.com/djdblxcxt/image/upload/v1712805043/business-class-cabin_vs0fmv.jpg"
+                link1="Know More"
+                img2="https://res.cloudinary.com/djdblxcxt/image/upload/v1712892777/premium-economy-class_valrld.png"
+                link2="Know More"
+                img3="https://res.cloudinary.com/djdblxcxt/image/upload/v1712764058/standard-economy-cabin_nkmmxk.png"
+                link3="Know More"
+            />
 
             <div className="flex items-center justify-center mt-16">
                 <div className="flex items-center justify-between gap-20">
                     <div className="border-2 border-gray-400 p-8 rounded-full hover:bg-blue-700 hover:text-white transition duration-500 cursor-pointer">
                         <GiNewspaper className='w-8 h-8' />
-
-                        {/* <div>
-                            <p><a href="/About/Press/Media.html" className="text-gray-600 font-semibold hover:text-blue-600 transition duration-300">Latest News</a></p>
-                        </div> */}
                     </div>
 
                     <div className="border-2 border-gray-400 p-8 rounded-full hover:bg-blue-700 hover:text-white transition duration-500 cursor-pointer">
                         <GiCargoCrate className='w-8 h-8' />
-
-                        {/* <div className="Info-Name">
-                            <p><a href="/About/Cargo/Cargo.html" className="text-gray-600 font-semibold hover:text-blue-600 transition duration-300">Cargo</a></p>
-                        </div> */}
                     </div>
 
                     <div className="border-2 border-gray-400 p-8 rounded-full hover:bg-blue-700 hover:text-white transition duration-500 cursor-pointer">
                         <FaQuestion className='w-8 h-8' />
-
-                        {/* <div className="Info-Name">
-                            <p><a href="/Support/FAQ/Query.html" className="text-gray-600 font-semibold hover:text-blue-600 transition duration-300">FAQ</a></p>
-                        </div> */}
                     </div>
 
                     <div className="border-2 border-gray-400 p-8 rounded-full hover:bg-blue-700 hover:text-white transition duration-500 cursor-pointer">
                         <IoCallOutline className='w-8 h-8' />
-
-                        {/* <div className="Info-Name">
-                            <p><a href="/Support/Help/Help.html" className="text-gray-600 font-semibold hover:text-blue-600 transition duration-300">Contact</a></p>
-                        </div> */}
                     </div>
                 </div>
             </div>
@@ -151,7 +162,7 @@ const Home = () => {
             <div>
                 <Header />
                 <AsideSection />
-                <PrepareToTravel />
+                <TravelInfo />
                 <Footer />
             </div>
         </>
